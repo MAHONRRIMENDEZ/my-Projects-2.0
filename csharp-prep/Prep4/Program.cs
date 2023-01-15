98e6ef3c934da39aadb07d1d75f7ef0e156a2ca6
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-01-14 23:22:56 -0500

    trying

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int UserNumber = -1;    
+        while(UserNumber ! = 0)
+        {
+            Console.Write("Please enter a number (0 to close the list): ");
+            String userResponse = Console.ReadLine();
+            UserNumber = int.Parse(userResponse);
+
+            if (UserNumber != 0)
+            {
+                numbers.Add(UserNumber);
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        Console.WriteLine($"sum result: {sum}");
+
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+
+
+        /////////////////////////////////////////
+        int max = numberss[0];
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+
+        Console.WriteLine($"The max is: {max}");
+
     }
 }
