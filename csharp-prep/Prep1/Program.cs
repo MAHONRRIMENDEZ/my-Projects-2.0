800f25d32b58c45fd232a96105dcaa7319f70837
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-01-14 22:21:40 -0500

    ASD

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,11 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What is your first name? ");
-        string Firstname = Console.ReadLine();
-        Console.Write("What is your last name my friend? ");
-        string Lastname =Console.ReadLine();
-
-        Console.WriteLine($"you are {Lastname}, {Firstname} {Lastname} ")
+        Console.WriteLine("Hello Prep1 World!");
     }
 }
