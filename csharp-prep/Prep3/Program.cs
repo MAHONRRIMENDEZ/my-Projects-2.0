a1ed701001fab261c39ffbc465d972a1206cd36f
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-01-14 22:52:36 -0500

    prep 3 completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,8 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("What will be yout magic number? ");
-        int UserNumber = Console.ReadLine();
-        int.Parse(UserNumber = );
+        //Console.Write("What will be yout magic number? ");
+        //int UserNumber = Console.ReadLine();
+        //int magicNumber = int.Parse(UserNumber);
+    
+
+    Random randomNumber = new Random ();
+    int magicNumber = randomNumber.Next(1, 101);
+
+    int Userguess = -1;
+    while (Userguess != magicNumber)
+    {
+        Console.Write("What do you think is the number? ");
+        Userguess = int.Parse(Console.ReadLine());
+
+        if (magicNumber > Userguess)
+        {
+            Console.WriteLine("the number is Higher");
+        }
+        else if (magicNumber < Userguess);
+        {
+            Console.Write("the number is lower");
+        }
+
+        else
+        {
+            Console.WriteLine("you got it!");
+        }
     }
+    }
+
 }
