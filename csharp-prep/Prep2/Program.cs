cf4bdaca92c9c96581daf157abcda39d48ba0447
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-01-14 22:07:35 -0500

    Prep 2 completed a second rep (corrected)

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string userAnswer = Console.ReadLine();
+        int percent = int.Parse(userAnswer);
+        
+        string userGradeLetter = "";
+
+        if (percent >= 90)
+        {
+            userGradeLetter = "A";
+        } 
+        else if (percent >= 80)
+        {
+            userGradeLetter = "B";
+        }
+        else if (percent >= 70)
+        {
+            userGradeLetter = "C";
+        }
+        else if (percent >= 60)
+        {
+            userGradeLetter = "D";
+        }
+        else
+        {
+            userGradeLetter = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {userGradeLetter}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("Congrats You've approved!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
