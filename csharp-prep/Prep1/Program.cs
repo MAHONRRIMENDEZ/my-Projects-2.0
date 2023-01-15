83118a878d7b3d3cf9ba9e8be4b5c7468e598718
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-01-14 22:14:28 -0500

    PREP 1 COMPLETED AGAIN

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What is your first name? ");
+        string Firstname = Console.ReadLine();
+        Console.Write("What is your last name my friend? ");
+        string Lastname =Console.ReadLine();
+
+        Console.WriteLine($"you are {Lastname}, {Firstname} {Lastname} ")
     }
 }
