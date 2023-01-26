6962a7704f7690fa1162520fde95162bdc04dcc0
Mahonrri Mendez <mahonrri.mendez47@gmail.com>
2023-01-25 22:34:54 -0500

    menu created

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,6 +4,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop02 World!");
+        Console.WriteLine("Please select one of the following");
+        Console.WriteLine("1. Write");
+        Console.WriteLine("2. Display");
+        Console.WriteLine("3. Load");
+        Console.WriteLine("4. Save");
+        Console.WriteLine("5. Quite");
+        Console.Write("what would you like to do?");
+        
     }
 }
